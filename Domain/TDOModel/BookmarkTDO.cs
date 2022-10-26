1976874a35de05861f559080f3f28a6e81b4cc9f
bonsserg@gmail.com <bonsserg@gmail.com>
2022-10-26 18:19:39 +0300

    update 0.4 added TDO model

---
+++ Domain/TDOModel/BookmarkTDO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookmarksAPI.Domain.TDOModel
+{
+    public class BookmarkTDO
+    {
+        public int Id { get; set; }
+        public DateTime DateСreation { get; set; }
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public long FolderId { get; set; }
+    }
+}
---
+++ Domain/TDOModel/FolderTDO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookmarksAPI.Domain.TDOModel
+{
+    public class FolderTDO
+    {
+        public long Id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public long UserId { get; set; }
+
+        public List<BookmarkTDO> BookMarks { get; set; }
+    }
+}
