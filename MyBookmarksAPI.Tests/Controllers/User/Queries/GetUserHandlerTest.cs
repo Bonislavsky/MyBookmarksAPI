6fb048fef9def2413f4864f2e4dfaeaca9cdc8ab
bonislavskys@gmail.com <bonislavskys@gmail.com>
2023-01-11 17:41:37 +0200

    0.27.5(changed folder structure in unit tests)

---
+++ MyBookmarksAPI.Tests/Controllers/User/Queries/GetUserHandlerTest.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyBookmarksAPI.Controllers;
+using MyBookmarksAPI.DAL;
+using MyBookmarksAPI.DAL.Wrapper;
+using MyBookmarksAPI.Domain.DtoModel.UserDtoModel;
+using MyBookmarksAPI.Domain.Helpers;
+using MyBookmarksAPI.Domain.Helpers.Mapping;
+using MyBookmarksAPI.Domain.Model;
+using MyBookmarksAPI.Service;
+using MyBookmarksAPI.Tests.Common;
+using Xunit;
+
+namespace MyBookmarksAPI.Tests.Controllers.User.Queries
+{
+    public class GetUserHandlerTest
+    {
+        private readonly Mapper _mapper = new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new AppMappingProfileUser());
+            cfg.AddProfile(new AppMappingProfileFolder());
+            cfg.AddProfile(new AppMappingProfileBookmark());
+        }));
+
+        private UserController Arrange(MyBookmarksDbContext dbContext)
+        {
+            var wrapper = new RepositoryWrapper(dbContext);
+            var userService = new UserService(wrapper);
+
+            return new UserController(userService, _mapper);
+        }
+
+        [Fact]
+        public async Task Get_ListUserAsync_ReturnsAllItems()
+        {
+            // Arrange
+            using var dbContext = UserContextFactory.Create(nameof(Get_ListUserAsync_ReturnsAllItems));
+            var controller = Arrange(dbContext);
+
+            // Act
+            var OkResult = await controller.GetUsers() as OkObjectResult;
+#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
+            var listUser = OkResult.Value as List<UserDto>;
+#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
+
+            // Assert
+            Assert.IsType<List<UserDto>>(listUser);
+            Assert.Equal(DataForTest.userList.Count, listUser.Count);
+        }
+
+        [Theory]
+        [InlineData(1, -1, long.MaxValue)]
+        public async Task Get_UserById_ReturnsUser(long id1, long id2, long id3)
+        {
+            // Arrange
+            using var dbContext = UserContextFactory.Create(nameof(Get_UserById_ReturnsUser));
+            var controller = Arrange(dbContext);
+
+            // Act
+            var OkResult = await controller.GetUser(id1) as OkObjectResult;
+#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
+            var user = OkResult.Value as UserDto;
+#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
+
+            var NotFound1 = await controller.GetUser(id2);
+            var NotFound2 = await controller.GetUser(id3);
+
+            // Assert
+            Assert.IsType<UserDto>(user);
+            Assert.True(user != null);
+            Assert.Equal(id1, user.Id);
+
+            Assert.IsType<NotFoundObjectResult>(NotFound1);
+            Assert.IsType<NotFoundObjectResult>(NotFound2);
+        }
+
+        [Theory]
+        [InlineData(1, -1, long.MaxValue)]
+        public async Task Get_AllDataUserById_ReturnsUser(long id1, long id2, long id3)
+        {
+            // Arrange
+            using var dbContext = UserContextFactory.Create(nameof(Get_AllDataUserById_ReturnsUser));
+            var controller = Arrange(dbContext);
+
+            // Act
+            var OkResult = await controller.GetAllDataUser(id1) as OkObjectResult;
+#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
+            var user = OkResult.Value as UserAllDataDto;
+#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
+
+            var NotFound1 = await controller.GetAllDataUser(id2);
+            var NotFound2 = await controller.GetAllDataUser(id3);
+
+            // Assert
+            Assert.IsType<UserAllDataDto>(user);
+            Assert.True(user != null);
+            Assert.Equal(id1, user.Id);
+
+            Assert.IsType<NotFoundObjectResult>(NotFound1);
+            Assert.IsType<NotFoundObjectResult>(NotFound2);
+        }
+
+
+    }
+}
